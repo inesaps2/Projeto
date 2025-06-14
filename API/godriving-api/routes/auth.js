ca3ae0a648d894dd1d9a039460675c07eb9eb83f
inesaps2 <164217711+inesaps2@users.noreply.github.com>
2025-06-14 16:31:46 +0100

    Create auth.js

---
+++ API/godriving-api/routes/auth.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+const connection = require('../db');
+const bcrypt = require('bcrypt');
+
+// Registo do utilizador
+router.post('/register', async (req, res) => {
+  const { name, email, password, id_type, category, associated_car } = req.body;
+
+  connection.query('SELECT * FROM user WHERE email = ?', [email], async (err, results) => {
+    if (err) return res.status(500).json({ error: 'Erro no banco' });
+    if (results.length > 0) return res.status(400).json({ error: 'Email já registado' });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    connection.query(
+      'INSERT INTO user (name, email, password, id_type, category, associated_car) VALUES (?, ?, ?, ?, ?, ?)',
+      [name, email, hashedPassword, id_type, category, associated_car],
+      (err, results) => {
+        if (err) return res.status(500).json({ error: 'Erro ao criar utilizador' });
+        res.status(201).json({ message: 'Usuário criado com sucesso!' });
+      }
+    );
+  });
+});
+
+// Login do utilizador
+router.post('/login', (req, res) => {
+  console.log('Dados recebidos:', req.body);
+  const { email, password } = req.body;
+
+  connection.query('SELECT * FROM user WHERE email = ?', [email], async (err, results) => {
+    if (err) return res.status(500).json({ error: 'Erro no banco' });
+    if (results.length === 0) return res.status(400).json({ error: 'Email não encontrado' });
+
+    const user = results[0];
+    console.log('Senha recebida:', password);
+    console.log('Hash armazenado:', user.password);
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    console.log('Password certa?', passwordMatch);
+
+    if (!passwordMatch) return res.status(401).json({ error: 'Senha incorreta' });
+
+    res.json({ message: 'Login efetuado com sucesso', user: { id: user.id, name: user.name, email: user.email, id_type: user.id_type } });
+  });
+});
+
+module.exports = router;
